4b594ad629818bec6a3ac1876df0d6c8ce4de8cc
David Svantesson <pelleitorp2@gmail.com>
2022-11-04 17:15:39 +0100

    update know you can se what day (name ) the date is.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
+
+public class App {
+    public static void main(String... args) {
+        LocalDate localDate = LocalDate.parse("2018-02-01");
+
+        ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZoneId.systemDefault());
+        Instant instant = zonedDateTime.toInstant();
+        Date date = Date.from(instant);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        DayOfWeek dayDate = localDate.getDayOfWeek();
+        System.out.println("LocalDate: " + localDate);
+        System.out.println("Calendar year: " + calendar.get(Calendar.YEAR));
+        System.out.println("Calendar month (0 to 11): " + calendar.get(Calendar.MONTH));// Calendar mont from 0 to 11
+        System.out.println("Calendar day of month: " + calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println("calender day name " + dayDate);
     }
+
+
+
 }
